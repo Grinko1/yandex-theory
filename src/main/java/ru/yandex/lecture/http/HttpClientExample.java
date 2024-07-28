ba8c3e74f50d3331f4274802b7b61fbe187a6adf
Grinko1 <nadenka64013730@gmail.com>
2024-07-28 13:10:39 +0900

    shows errors, statuses, Gson

--- src/main/java/ru/yandex/lecture/http/HttpClientExample.java
+++ src/main/java/ru/yandex/lecture/http/HttpClientExample.java
@@ -1,5 +1,8 @@
 package ru.yandex.lecture.http;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -8,20 +11,46 @@ import java.net.http.HttpResponse;
 
 public class HttpClientExample {
     public static void main(String[] args) throws IOException, InterruptedException {
-        URI uri = URI.create("https://httpstatuses.com/418");
+        URI uri = URI.create("http://ipwhois.app/json/46.226.227.20?lang=ru");
 
-        HttpRequest.Builder requestBuilder= HttpRequest.newBuilder();
+        HttpRequest.Builder requestBuilder = HttpRequest.newBuilder();
         HttpRequest request = requestBuilder
-                .GET()
+
                 .uri(uri)
-                .version(java.net.http.HttpClient.Version.HTTP_1_1)
-                .header("Accept", " text/html")
+                .GET()
+//                .version(java.net.http.HttpClient.Version.HTTP_1_1)
+//                .header("Accept", " text/html")
+//                .header("Accept", " application/xml")
+//                .header("Accept", " application/json")
                 .build();
+
+
         HttpClient client = HttpClient.newHttpClient();
+
+
         HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
-        HttpResponse<String> response = client.send(request, handler);
-        System.out.println("Code response "+ response.statusCode());
-        System.out.println("Body of response "+ response.body());
+
+        try {
+            HttpResponse<String> response = client.send(request, handler);
+            int status = response.statusCode();
+            System.out.println("Code response " + status);
+        if (status == 200){
+            JsonElement jsonElement = JsonParser.parseString(response.body());
+            if (!jsonElement.isJsonObject()){
+                System.out.println("wrong response");
+                return;
+            }
+        }
+            if (status >= 200 && status <= 299){
+                System.out.println("success");
+                System.out.println("Body of response " + response.body());
+            }
+
+
+        } catch (IOException | InterruptedException e) {
+            System.out.println("Something went wrong");
+        }
+
     }
 }
 
